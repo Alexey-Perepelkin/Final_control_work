c04ac402dfec1026c4eabb85d0611f03ae9af22e
Perepelkin Alexey <kip_a@list.ru>
2023-04-09 19:28:13 +0300

    Creating version_2

---
+++ Control_Task_2/Program.cs
@@ -0,0 +1,49 @@
+﻿string[] inputDates(int size)
+{
+    string[] arrValue = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите <{i+1}> элемент массива:\t");
+        arrValue[i] = Console.ReadLine()!;
+    }
+    return arrValue;
+}
+void PrintArr(string[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.WriteLine(arr[i]);
+}
+string[] OutputDates(string[] arr, int lenghtString)
+{
+    int newSizeArr = 0;
+    int sizeArr = arr.Length;
+    int LenghtVal = lenghtString;
+    int i = 0;
+
+    for (i = 0; i < sizeArr; i++)
+        if (arr[i].Length <= LenghtVal) newSizeArr++;
+    string[] newDates = new string[newSizeArr];
+
+    int indexNewDates = 0;
+    for (i = 0; i < sizeArr; i++)
+        if (arr[i].Length <= LenghtVal)
+        {
+            newDates[indexNewDates] = arr[i];
+            indexNewDates++;
+        }
+      return  newDates;  
+}
+Console.Clear();
+
+Console.Write("Введите размерност массива:\t");
+int size = int.Parse(Console.ReadLine()!);
+
+string[] arrValue = inputDates(size);
+
+Console.Write("Колличество символов элемента массива больше или равно:\t");
+int lenghtElement = int.Parse(Console.ReadLine()!);
+
+string[] arrOutput = OutputDates(arrValue,lenghtElement);
+
+PrintArr(arrOutput);
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Final_control_work
-This is a test for a programming cours.
-1. A commit "Creating a project".
-Adding a new folder. Folder name Control_task.
-2. A commit "version 1.0". The code with a static array of primary date without using methods.
+### Контрольная работа по курсу программирование
+## Ветка Main. 
+### Создание кода со статическим массивом , без использования методов
+- Commit "version_1.0" Сщздание кода
+
+## Ветка version_2
+- Создание кода с использованием методов.
